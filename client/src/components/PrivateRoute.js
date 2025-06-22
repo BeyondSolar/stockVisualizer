b18f28c8f0237365c4d3e430e69aab222947411c
Abhishek Yadav <abhicsjuet@gmail.com>
2025-06-22 16:29:35 +0530

    4.0.4 private route bug fixed

--- client/src/components/PrivateRoute.js
+++ client/src/components/PrivateRoute.js
@@ -2,8 +2,8 @@ import { Navigate } from 'react-router-dom';
 import { useAuth } from '../context/authContext';
 
 function PrivateRoute({ children }) {
-  const { user } = useAuth();
-  return user ? children : <Navigate to="/login" />;
+  const { token } = useAuth();
+  return token ? children : <Navigate to="/login" />;
 }
 
 export default PrivateRoute;
