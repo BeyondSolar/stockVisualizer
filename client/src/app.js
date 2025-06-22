444a1f15aff047d39960f2e27f9c5c9cfa65ca52
Abhishek Yadav <abhicsjuet@gmail.com>
2025-06-22 07:12:52 +0530

    4.0.3 Private Routes added

--- client/src/app.js
+++ client/src/app.js
@@ -7,18 +7,21 @@ import Portfolio from './components/Portfolio';
 import Market from './components/Market';
 import Profile from './components/Profile';
 import Transactions from './components/Transactions';
+import PrivateRoute from './components/PrivateRoute';
 
 const AppRoutes = () => (
   <Routes>
     <Route path="/login" element={<Login />} />
     <Route path="/register" element={<Register />} />
 
-    <Route path="/" element={<Layout />}>
-      <Route index element={<Dashboard />} />
-      <Route path="/portfolio" element={<Portfolio />} />
-      <Route path="/market" element={<Market />} />
-      <Route path="/transactions" element={<Transactions />} />
-      <Route path="/profile" element={<Profile />} />
+    <Route element={<PrivateRoute />}>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Dashboard />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="/market" element={<Market />} />
+          <Route path="/transactions" element={<Transactions />} />
+          <Route path="/profile" element={<Profile />} />
+        </Route>
     </Route>
   </Routes>
 );
